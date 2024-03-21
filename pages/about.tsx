374e254545e7c5e1471f8998048901c8ec1c8090
macarah <115976408+macarah@users.noreply.github.com>
2024-03-21 02:56:50 -0400

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,15 +10,13 @@ export default function About() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       <div className="relative">
-        <p className="font-mono text-sm">In a world where cultural imperialism once reigned supreme in the media industry, Netflix has emerged as a disruptive force, dominating the global streaming landscape. Amanda D Lotz's 2022 journal, Netflix, library analysis, and globalization: rethinking mass media flows, sheds light on Netflix's unique approach to content sourcing and circulation, highlighting its data-centric strategy that optimizes foreign show and movie recommendations. By analyzing the popularity and genres of reality dating shows across the world, I aim to uncover the intricate dynamics of transnational media flows in the streaming era.</p>
+        <p className="font-mono text-sm">In a world where cultural imperialism once reigned supreme in the media industry, Netflix has emerged as a disruptive force, dominating the global streaming landscape. Amanda D Lotz&apos;s 2022 journal, Netflix, library analysis, and globalization: rethinking mass media flows, sheds light on Netflix&apos;s unique approach to content sourcing and circulation, highlighting its data-centric strategy that optimizes foreign show and movie recommendations. By analyzing the popularity and genres of reality dating shows across the world, I aim to uncover the intricate dynamics of transnational media flows in the streaming era.</p>
         <br /><br />
-        <p className="font-mono text-sm">The study will delve into the top 10 shows in the genre of comedy from January to June 2023, evaluating their global popularity and duration in the top rankings. Additionally, this study seeks to differentiate cultural actions & contexts surrounding comedy by analyzing a corpus of transcripts. By comparing these findings, I seek to discern patterns of audience preferences and cultural resonance, providing valuable insights into the evolving landscape of global entertainment consumption. 
+        <p className="font-mono text-sm">The study will delve into the top 10 shows in the genre of comedy from January to June 2023, evaluating their global popularity and duration in the top rankings. Additionally, this study seeks to differentiate cultural actions &amp; contexts surrounding comedy by analyzing a corpus of transcripts. By comparing these findings, I seek to discern patterns of audience preferences and cultural resonance, providing valuable insights into the evolving landscape of global entertainment consumption. 
         </p>
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
       </div>
-
-      
     </main>
   );
 }
